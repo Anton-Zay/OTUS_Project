4bdd9f94f4775d64c9921767628dc3a404f0fba0
Anton-Zay <zay-toxa@mail.ru>
2022-01-05 21:35:55 +0300

    1st iteration of project. Without database.

---
+++ Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSantaBot
+{
+    public static class Config
+    {
+        public static string Token { get; set; } = "5095584182:AAGrGSLjY0lAKfRhKaD8YqYK_ALVC7tDe60";
+        public static string BotName { get; set; } = "secret.santaBot";
+        public static string BotUserName { get; set; } = "secret_joulupukki_Bot";
+    }
+}
---
+++ Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSantaBot
+{
+    class Entity
+    {
+        public int ID { get; set; } 
+        public long Chat_Id { get; set; }
+        public long User_Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Wish { get; set; }
+        public bool IsActive { get; set; }
+        public Entity SantaFor { get; set; } = null; // ссылка тому кому дарит
+        public Entity Gifted { get; set; } = null; // ссылка на того кто дарит этому
+    }
+}
